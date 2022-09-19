4cea9a5e9ab7d49523d781589fd87fdef8e2ef91
Your Name <you@example.com>
2022-09-19 16:32:38 -0300

    Cria o Projeto

---
+++ android/app/src/main/kotlin/com/example/magicalworld/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.magicalworld
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: GridView(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              childAspectRatio: 0.6),
+          children: [
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.red,
+            ),
+            Container(
+              color: Colors.red,
+            ),
+          ]),
+    );
+  }
+}
