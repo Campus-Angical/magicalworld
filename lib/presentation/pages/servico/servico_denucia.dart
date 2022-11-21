2c7fcb8ce855f9ed7d8fd33ff6b01f5d03b06428
Your Name <you@example.com>
2022-11-21 16:11:30 -0300

    Função de favoritar serviços

--- lib/presentation/pages/servico/servico_denucia.dart
+++ lib/presentation/pages/servico/servico_denucia.dart
@@ -17,77 +17,79 @@ class _DenuciaPageState extends State<DenuciaPage> {
       appBar: AppBar(
         backgroundColor: Color.fromRGBO(242, 145, 208, 1),
       ),
-      body: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: Text(
-                  'Denúncia',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            width: 90,
-            child: Divider(
-              thickness: 3,
-              color: Color.fromRGBO(242, 145, 208, 1),
-            ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            child: TextFormField(
-              keyboardType: TextInputType.text,
-              maxLines: null,
-              maxLength: 1000,
-              decoration: InputDecoration(
-                hintText: 'Informe sua Reclamação',
-                prefixIcon: Icon(
-                  Icons.sms_rounded,
-                  color: Colors.black,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: Color.fromARGB(255, 247, 122, 236), width: 2),
-                ),
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () {},
-                child: Padding(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
                   padding: const EdgeInsets.only(top: 50),
-                  child: Container(
-                    width: 200,
-                    height: 60,
-                    decoration: BoxDecoration(
-                        color: Color.fromRGBO(242, 145, 208, 1),
-                        borderRadius: BorderRadius.circular(5)),
-                    child: Center(
-                      child: Text(
-                        'Enviar Denúncia',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
+                  child: Text(
+                    'Denúncia',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              width: 90,
+              child: Divider(
+                thickness: 3,
+                color: Color.fromRGBO(242, 145, 208, 1),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: TextFormField(
+                keyboardType: TextInputType.text,
+                maxLines: null,
+                maxLength: 1000,
+                decoration: InputDecoration(
+                  hintText: 'Informe sua Reclamação',
+                  prefixIcon: Icon(
+                    Icons.sms_rounded,
+                    color: Colors.black,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromARGB(255, 247, 122, 236), width: 2),
+                  ),
+                  border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(5)),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    child: Container(
+                      width: 200,
+                      height: 60,
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(242, 145, 208, 1),
+                          borderRadius: BorderRadius.circular(5)),
+                      child: Center(
+                        child: Text(
+                          'Enviar Denúncia',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              )
-            ],
-          )
-        ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
